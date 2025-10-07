99d6e421775d1b6edd7cb4c996641da5bc40ceeb
rs379 <rs379@student.waikato.ac.nz>
2025-10-07 23:28:39 +1300

    commit 4: created another section in the homepage

--- alfiescafe/src/pages/homepage.tsx
+++ alfiescafe/src/pages/homepage.tsx
@@ -1,9 +1,33 @@
 import Slideshow from "../components/slideshow";
+import allergy_icon from '../assets/imgs/allergy_icon.svg'
+import gf_icon from '../assets/imgs/gf_icon.svg'
+import milk_icon from '../assets/imgs/milk_icon.svg'
 
-function Homepage(){
+import '../css/homepage.css'
+
+function Homepage() {
     return (
         <div className="homepage_wrapper">
-            <Slideshow />
+            <div className="slideshow_wrapper">
+                <Slideshow />
+            </div>
+            <div className="icons_wrapper">
+                <div>
+                    <img className="allergy_icon" src={allergy_icon} />
+                    <h1>allergies</h1>
+                    <p>Please tell us any allergys you may have and we will do our best to accomidate to you.</p>
+                </div>
+                <div>
+                    <img className="gf_icon" src={gf_icon} />
+                    <h1>Gluten Free</h1>
+                    <p>We have many gluten free options, from the cabinet to the menu, but please let the staff know so we can show you</p>
+                </div>
+                <div>
+                    <img className="milk_icon" src={milk_icon} />
+                    <h1>Alternative Milk</h1>
+                    <p>We have many alternative milk; Almond, Soy, Coconut, Oat, and Trim milk.</p>
+                </div>
+            </div>
         </div>
     )
 }
