c2c6df5b4eb6b84172502e76dda0d578413ee0ef
rs379 <rs379@student.waikato.ac.nz>
2025-10-10 22:10:48 +1300

    Commit 8: homepage UPDATE: added new section of food images, and changed the matcha content to section, but have not changed the name of the divs and css

--- alfiescafe/src/pages/homepage.tsx
+++ alfiescafe/src/pages/homepage.tsx
@@ -2,15 +2,22 @@ import Slideshow from "../components/slideshow";
 import allergy_icon from '../assets/imgs/allergy_icon.svg'
 import gf_icon from '../assets/imgs/gf_icon.svg'
 import milk_icon from '../assets/imgs/milk_icon.svg'
+import img_coffee from '../assets/imgs/coffee_nice.jpg'
+import img_waffles from '../assets/imgs/waffles.jpg'
+import img_scone from '../assets/imgs/scone.jpg'
+import img_cake from '../assets/imgs/cake.jpg'
+
 
 import '../css/homepage.css'
 
 function Homepage() {
     return (
         <div className="homepage_wrapper">
+            {/* SLIDESHOW SECTION */}
             <div className="slideshow_wrapper">
                 <Slideshow />
             </div>
+            {/* ICONS SECTION */}
             <div className="icons_wrapper">
                 <div>
                     <img className="allergy_icon" src={allergy_icon} />
@@ -28,10 +35,18 @@ function Homepage() {
                     <p>We have many alternative milk; Almond, Soy, Coconut, Oat, and Trim milk.</p>
                 </div>
             </div>
+            {/* MATCHA SECTION */}
             <div className="matcha_wrapper">
                 <div className="matcha_title">
-                    <h1 className="matcha_title_h1">try our new matcha!!!</h1>
+                    <h1 className="matcha_title_h1">1 North City Road, Rototuna North</h1>
                 </div>
+            </div>
+            {/* FOOD IMAGES SECTION */}
+            <div className="HP_FoodImages_wrapper">
+                <img className="HP_foodimages" src={img_cake}/>
+                <img className="HP_foodimages" src={img_waffles}/>
+                <img className="HP_foodimages" src={img_scone}/>
+                <img className="HP_foodimages" src={img_coffee}/>
             </div>
         </div>
     )
